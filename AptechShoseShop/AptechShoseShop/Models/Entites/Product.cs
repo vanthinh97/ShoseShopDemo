184f1bd96682b51b788827e9e30a0a860030e7d4
intern.thinh <thinhdv.codecomplete@gmail.com>
2021-03-08 21:42:36 +0700

    demo website

---
+++ AptechShoseShop/AptechShoseShop/Models/Entites/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptechShoseShop.Models.Entites
+{
+    public class Product
+    {
+        public Product()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+            ProductColors = new HashSet<ProductColor>();
+            ProductImages = new HashSet<ProductImage>();
+            ProductSizes = new HashSet<ProductSize>();
+        }
+
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public double DiscountRatio { get; set; }
+        public DateTime DiscountExpiry { get; set; }
+        public int CategoryId { get; set; }
+        public int? ProductImageId { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public int StatusId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual TbUser CreatedByNavigation { get; set; }
+        public virtual ProductImage ProductImage { get; set; }
+        public virtual StatusProduct Status { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+        public virtual ICollection<ProductColor> ProductColors { get; set; }
+        public virtual ICollection<ProductImage> ProductImages { get; set; }
+        public virtual ICollection<ProductSize> ProductSizes { get; set; }
+    }
+}
---
+++ AptechShoseShop/AptechShoseShop/Models/Entites/ProductImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptechShoseShop.Models.Entites
+{
+    public class ProductImage
+    {
+        public ProductImage()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
